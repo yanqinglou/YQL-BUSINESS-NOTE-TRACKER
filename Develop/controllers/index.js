99c62d8838c60c24d63388f5219d129e949e38de
ber <ber@berdeMacBook-Pro.local>
2023-01-30 11:45:49 -0800

    save function is set up

--- Develop/controllers/index.js
+++ Develop/controllers/index.js
@@ -1,16 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const path = require("path");
 
 router.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-  });
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
 router.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/notes.html"));
-  });
-  
+  res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
+
+
 const itemRoutes = require("./itemscontrollers");
-router.use("/api/notes",itemRoutes);
+router.use("/api/notes", itemRoutes);
 
 module.exports = router;
